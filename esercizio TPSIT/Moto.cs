c0a6f02dfef96b1e6d1be573ff2b60706b535ac7
riccardoneeeh <riccardo.santoni@studenti.isissgobetti.it>
2025-10-27 16:53:22 +0100

    Update Moto.cs

--- esercizio TPSIT/Moto.cs
+++ esercizio TPSIT/Moto.cs
@@ -1,8 +1,25 @@
-﻿using System;
 
-public class moto
+using System;
+public class Moto : Veicolo
 {
-	public moto ()
-	{
-	}
+    // Proprietà specifiche della moto
+    public bool HaSidecar { get; set; }
+    public int Cilindrata { get; set; }
+
+    // Costruttore
+    public Moto(string marca, string modello, int anno, string colore, int cilindrata, bool haSidecar)
+        : base(marca, modello, anno, colore)
+    {
+        Cilindrata = cilindrata;
+        HaSidecar = haSidecar;
+    }
+
+    // Metodo per descrivere la moto
+    public new void Descrivi()
+    {
+        base.Descrivi();
+        Console.WriteLine($"Cilindrata: {Cilindrata}cc, Sidecar: {(HaSidecar ? "Sì" : "No")}");
+    }
 }
+
+
