637af0e790c70ba719ef1f1eafd1f9276936f0f2
riccardoneeeh <riccardo.santoni@studenti.isissgobetti.it>
2025-10-27 16:53:55 +0100

    Update Veicolo.cs

--- esercizio TPSIT/Veicolo.cs
+++ esercizio TPSIT/Veicolo.cs
@@ -1,11 +1,26 @@
-﻿using System;
 
+using system;
 public class Veicolo
 {
-	public Veicolo ()
-	{
-		public class Veicolo
-        Veicolo=()
+    // Proprietà
+    public string Marca { get; set; }
+    public string Modello { get; set; }
+    public int Anno { get; set; }
+    public string Colore { get; set; }
 
-	}
+    // Costruttore
+    public Veicolo(string marca, string modello, int anno, string colore)
+    {
+        Marca = marca;
+        Modello = modello;
+        Anno = anno;
+        Colore = colore;
+    }
+
+    // Metodo per descrivere il veicolo
+    public void Descrivi()
+    {
+        Console.WriteLine($"Veicolo: {Marca} {Modello}, Anno: {Anno}, Colore: {Colore}");
+    }
 }
+
